2e8df6181a86aecd30b94ee07c14aa5386ec4a2e
Kerry Muniz <kerrym877@gmail.com>
2024-01-05 22:02:12 -0300

    criando meu objeto Quarto no pratica dos conteudos estudados sobre OO

---
+++ PHP OO/Prática dos conteúdos/arquivo_pratica.php
@@ -0,0 +1,32 @@
+<?php
+
+    namespace PraticarConteudo;
+
+    class Quarto {
+        public $bancada;
+        public $janela;
+        private $cama;
+        protected $guardaRoupa;
+
+        function __construct($bancada, $janela, $cama, $guardaRoupa)
+        {
+            $this->bancada = $bancada;
+            $this->janela = $janela;
+            $this->cama = $cama;
+            $this->guardaRoupa = $guardaRoupa;
+        }
+
+        //métodos mágicos
+        function __set($attr, $value)
+        {
+            $this->$attr = $value;
+        }
+        function __get($attr)
+        {
+            return $this->$attr;
+        }
+    }
+
+    $quarto = new PraticarConteudo\Quarto;
+
+?>
