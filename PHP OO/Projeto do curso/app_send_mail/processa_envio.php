faed42d059bd9c7635597d4d7207fdf93b217d80
Kerry Muniz <kerrym877@gmail.com>
2024-01-10 10:24:57 -0300

    criando o objeto mensagem e instanciando ele + setando os atributos com valor dos indices da $_POST

--- PHP OO/Projeto do curso/app_send_mail/processa_envio.php
+++ PHP OO/Projeto do curso/app_send_mail/processa_envio.php
@@ -4,4 +4,32 @@
     print_r($_POST);
     echo '<pre>';
 
+    class Mensagem {
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
+
+        public function __get($attr)
+        {
+            return $this->$attr;
+        }
+
+        public function __set($attr, $value)
+        {
+            $this->$attr = $value;
+        }
+
+        public function mensagemValida()
+        {
+
+        }
+    }
+
+    $mensagem = new Mensagem();
+
+    $mensagem->__set('para', $_POST['para']);
+    $mensagem->__set('assunto', $_POST['assunto']);
+    $mensagem->__set('mensagem', $_POST['mensagem']);
+    print_r($mensagem);
+
 ?>
