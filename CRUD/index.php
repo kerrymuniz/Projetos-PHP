1b082b64e630e0f9da269efe8c173bc871a10d0e
Kerry Muniz <kerrym877@gmail.com>
2024-02-10 19:54:40 -0300

    inclusão do bootstrap 5 no projeto para facilitar no desenvolvimento front-end  + ajustes das importações dos links

---
+++ CRUD/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Cadastro</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+
+  </body>
+</html>
