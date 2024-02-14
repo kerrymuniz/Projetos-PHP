0b23a619b7bb791b11e4f8f3794b142e262d7702
Kerry Muniz <kerrym877@gmail.com>
2024-02-14 15:45:37 -0300

    tratando a finalização do cadastro caso seja bem sucedido ou não

--- CRUD/salvar-usuario.php
+++ CRUD/salvar-usuario.php
@@ -10,6 +10,14 @@
             $sql = "INSERT INTO usuarios(Nome, Email, Senha, Data_nasc) VALUES('{$nome}', '{$email}', '{$senha}', '{$data_nasc}')";
 
             $result = $conexao->query($sql);
+
+            if($result == true) {
+                print "<script>alert('Cadastro realizado com sucesso');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            } else {
+                print "<script>alert('Não foi possível realizar o cadastro. Por favor tentar novamente..');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }
             break;
         case 'editar':
             //code
