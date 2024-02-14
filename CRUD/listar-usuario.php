964013420f93c6e82d8d82a2fe87a1dca20126ce
Kerry Muniz <kerrym877@gmail.com>
2024-02-14 17:23:48 -0300

    teste condicional

--- CRUD/listar-usuario.php
+++ CRUD/listar-usuario.php
@@ -5,14 +5,18 @@
 
     $result = $conexao->query($sql); //executando a query a partir da conexao
 
-    $qtd = $result->num_rows; //retornando o número de linhas do ressultado da consulta
+    if($result) {
+        $qtd = $result->num_rows; //retornando o número de linhas do ressultado da consulta
 
-    if($qtd > 0) {
-        while($row = $result->fetch_object()) {
-            print $row->id;
-            print $row->nome;
-            print $row->email;
-            print $row->data_nasc;
+        if($qtd > 0) {
+            while($row = $result->fetch_object()) {
+                print $row->id;
+                print $row->nome;
+                print $row->email;
+                print $row->data_nasc;
+            }
+        } else {
+            print "<p class='alert alert-danger'>Não foi possível encontrar resultados!</p>";
         }
     } else {
         print "<p class='alert alert-danger'>Não foi possível encontrar resultados!</p>";
