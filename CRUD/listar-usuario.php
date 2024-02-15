6135502338421cc4d51e3d3b82102372f2700d37
Kerry Muniz <kerrym877@gmail.com>
2024-02-14 23:07:24 -0300

    finalizando a parte de listagem dos usuários do projeto e iniciando ações de update e delete

--- CRUD/listar-usuario.php
+++ CRUD/listar-usuario.php
@@ -3,8 +3,36 @@
 
     $sql = "SELECT * FROM usuarios"; //query selecionando todos os itens da tabela usuarios
 
-    $result = $conexao->query($sql); //executando a query a partir da conexao
+    $result = mysqli_query($conexao, $sql); //executando a query a partir da conexao
 
-    $qtd = $result->num_rows; //retornando o número de linhas do ressultado da consulta
+    $qtd = mysqli_num_rows($result); //retornando o número de linhas do ressultado da consulta
+
+    if($qtd > 0)
+    {
+        echo "<table class='table table-striped table-bordered table-hover'>";
+                echo "<tr>";
+                echo "<th>#</th>";
+                echo "<th>Nome</th>";
+                echo "<th>Email</th>";
+                echo "<th>Data de Nascimento</th>";
+                echo "<th>Ações</th>";
+                echo "</tr>";
+            while($row = mysqli_fetch_array($result))
+            {
+                echo "<tr>";
+                echo "<td>" . $row["Id"] . "</td>";
+                echo "<td>" . $row["Nome"] . "</td>";
+                echo "<td>" . $row["Email"] . "</td>";
+                echo "<td>" . $row["Data_nasc"] . "</td>";
+                echo "<td>
+                        <button oneclick=\"location.href='?page=editar&id=". $row["Id"] . "';\" class='btn btn-success'>Editar</button>
+                        <button class='btn btn-danger'>Excluir</button>
+                    </td>";
+                echo "</tr>";
+            }
+        echo "</table>";
+    } else {
+        echo "<p class='alert alert-danger'>Não foi possível encontrar resultados!</p>";
+    }
 
 ?>
