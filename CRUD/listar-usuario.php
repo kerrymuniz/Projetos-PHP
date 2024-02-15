96ff4d6d059ce2b1045ec8b9019643ec1ff35559
Kerry Muniz <kerrym877@gmail.com>
2024-02-14 21:15:54 -0300

    ajuste em listar-usuarios

--- CRUD/listar-usuario.php
+++ CRUD/listar-usuario.php
@@ -5,21 +5,6 @@
 
     $result = $conexao->query($sql); //executando a query a partir da conexao
 
-    if($result) {
-        $qtd = $result->num_rows; //retornando o número de linhas do ressultado da consulta
-
-        if($qtd > 0) {
-            while($row = $result->fetch_object()) {
-                print $row->id;
-                print $row->nome;
-                print $row->email;
-                print $row->data_nasc;
-            }
-        } else {
-            print "<p class='alert alert-danger'>Não foi possível encontrar resultados!</p>";
-        }
-    } else {
-        print "<p class='alert alert-danger'>Não foi possível encontrar resultados!</p>";
-    }
+    $qtd = $result->num_rows; //retornando o número de linhas do ressultado da consulta
 
 ?>
